5a94fe328dad7ce796d6a3250479414bcaa07381
ChingHuaNing <angeline.999@1utar.my>
2021-04-01 01:47:56 +0800

    (Homepage) added carousel - image gallery

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ImageCarousel from "./ImageCarousel";
 
 function Example() {
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
+                <ImageCarousel />
             </div>
         </div>
     );
@@ -19,6 +14,6 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById("example")) {
+    ReactDOM.render(<Example />, document.getElementById("example"));
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,39 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Book') }}</div>
-                    <div class="card-body">
-                        Welcome to FS Book Store Web Application.
-                        @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                        </div>
-                        @endif
-
-                        @can('isAdmin')
-                        <div class="btn btn-success btn-lg">
-                        You have Admin Access
-                        </div>
-                        @else
-                        <div class="btn btn-info btn-lg">
-                        You have User Access
-                        </div>
-                        @endcan
-                    </div>
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-
-                </div>
-            </div>
+            <div id="example"></div>
+            <script src="/js/app.js"></script>
         </div>
     </div>
 @endsection
